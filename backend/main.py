daccdb1df0ec3d3071e64f6085f05712c9d87b43
Divy Vinayak Diwedi <divyvinayak@gmail.com>
2024-06-10 22:12:01 +0530

    file upload frontend and backend ready. Next up is openCV use

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,32 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    # got the file here
+    # process image file -> extract the result schema -> send back to user
+    return {"filename": file.filename}
--- frontend/src/components/UploadPhoto.tsx
+++ frontend/src/components/UploadPhoto.tsx
@@ -1,44 +1,60 @@
-import { Result } from '../types';
-import { ChangeEvent, useState } from 'react';
+import { Result } from "../types";
+import { ChangeEvent, useState } from "react";
 
-export function UploadPhoto({setResult}: {setResult: React.Dispatch<React.SetStateAction<Result | null>>}) {
-  const [file, setFile] = useState<File>();
-  const [fileUrl, setFileUrl] = useState<string | null>(null);
-  
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-      setFileUrl(URL.createObjectURL(e.target.files[0]))
-    }
-  };
+export function UploadPhoto({
+    setResult,
+}: {
+    setResult: React.Dispatch<React.SetStateAction<Result | null>>;
+}) {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [selectedFileUrl, setSelectedFileUrl] = useState<string | null>(null);
 
-  const handleUploadClick = () => {
-    if (!file) {
-      return;
-    }
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setSelectedFile(event.target.files[0]);
+            setSelectedFileUrl(URL.createObjectURL(event.target.files[0]));
+        }
+    };
 
-    fetch('https://httpbin.org/post', {
-      method: 'POST',
-      body: file,
-      headers: {
-        'content-type': file.type,
-        'content-length': `${file.size}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log({data});
-        setResult(data)
-      })
-      .catch((err) => console.error(err));
-  };
+    const handleUpload = () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append("file", selectedFile);
 
-  return (
-    <div className='flex flex-col gap-2'>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUploadClick} className='border-[1px] border-black p-2 rounded-md bg-gray-500 text-white hover:bg-gray-400'>Upload</button>
-      {/* <div>{file && `${file.name} - ${file.type}`}</div> */}
-      {fileUrl ? <img src={fileUrl} className='w-20'/> : "Preview of Chosen Photo"}
-    </div>
-  );
+            fetch("http://localhost:8000/uploadfile/", {
+                method: "POST",
+                headers: {
+                    accept: "application/json",
+                },
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setResult(data);
+                    console.log(data);
+                })
+                .catch((error) => console.error("Error:", error));
+        } else {
+            console.error("No file selected");
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-1/4">
+                <input type="file" onChange={handleFileChange} />
+                <button
+                    className="border-[1px] border-gray-600 bg-teal-500 hover:bg-teal-400 p-2 rounded-md"
+                    onClick={handleUpload}
+                >
+                    Upload
+                </button>
+            </div>
+            {selectedFileUrl ? (
+                <img src={selectedFileUrl} className="w-20" />
+            ) : (
+                "Selected Image Preview"
+            )}
+        </div>
+    );
 }
